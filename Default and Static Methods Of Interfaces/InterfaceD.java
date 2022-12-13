7cd784e9b288b8f2e93a45acdd38dd1407fbe369
yshaia7 <yshaia7@gmail.com>
2022-12-13 23:52:41 +0200

    First commit

---
+++ Default and Static Methods Of Interfaces/InterfaceD.java
@@ -0,0 +1,8 @@
+package com.modernjava.defaults;
+
+public interface InterfaceD {
+
+    default void sumA(int num1, int num2){
+        System.out.println("InterfaceA.sumA " + (num1 + num2));
+    }
+}
